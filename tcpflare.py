e4f5469e128e068742dded03bbc144257b9712a9
Floribunda <81042565+WisteriaFloribunda@users.noreply.github.com>
2023-10-11 13:26:43 +0000

    env file creation and loading

--- tcpflare.py
+++ tcpflare.py
@@ -1,6 +1,15 @@
 import discord
 import os
 from dotenv import load_dotenv
+import sys
+
+if not os.path.exists('.env'):
+    with open('.env', 'w') as env_file:
+        env_file.write('# ここに環境変数を設定\nDISCORD_TOKEN=')
+    print('環境変数ファイルを作成しました。必要な情報を入力してください')
+    sys.exit(1)
+
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+
+
